d9843e7ab32a8b6afde02cf573da1bc3aec19b01
hong jun ki <ghdwnsrl1991@gmail.com>
2024-09-11 14:17:39 +0900

    init.

---
+++ src/main/java/junki/fishkeepingback/FishKeepingBackApplication.java
@@ -0,0 +1,13 @@
+package junki.fishkeepingback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishKeepingBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FishKeepingBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/junki/fishkeepingback/FishKeepingBackApplicationTests.java
@@ -0,0 +1,13 @@
+package junki.fishkeepingback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishKeepingBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
