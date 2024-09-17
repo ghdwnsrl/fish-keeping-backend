a97b5a0845b815005d9f05514562563071b34ef5
hong jun ki <ghdwnsrl1991@gmail.com>
2024-09-17 20:16:21 +0900

    feat. User Entity 추가

---
+++ src/main/java/junki/fishkeepingback/domain/user/User.java
@@ -0,0 +1,34 @@
+package junki.fishkeepingback.domain.user;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@Getter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/junki/fishkeepingback/domain/user/UserService.java
@@ -0,0 +1,20 @@
+package junki.fishkeepingback.domain.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@RequiredArgsConstructor
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public void join(JoinReq joinReq) {
+        String encodedPw = passwordEncoder.encode(joinReq.getPassword());
+        User user = new User(joinReq.getUsername(), encodedPw);
+        userRepository.save(user);
+    }
+}
