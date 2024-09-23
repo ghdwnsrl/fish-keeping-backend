94a13fc7da028c18b48c1c5f1cd5a9234f2d04fc
hong jun ki <ghdwnsrl1991@gmail.com>
2024-09-23 19:03:05 +0900

    feat. BaseEntity를 위한 Annotation 추가

--- src/main/java/junki/fishkeepingback/FishKeepingBackApplication.java
+++ src/main/java/junki/fishkeepingback/FishKeepingBackApplication.java
@@ -2,8 +2,10 @@ package junki.fishkeepingback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class FishKeepingBackApplication {
 
     public static void main(String[] args) {
