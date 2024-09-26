e3e8e29656b9471f89b093101da9d05c8b703fd1
hong jun ki <ghdwnsrl1991@gmail.com>
2024-09-26 11:51:07 +0900

    refact. postId field 제거

--- src/main/java/junki/fishkeepingback/domain/comment/dto/CommentReq.java
+++ src/main/java/junki/fishkeepingback/domain/comment/dto/CommentReq.java
@@ -3,5 +3,5 @@ package junki.fishkeepingback.domain.comment.dto;
 import jakarta.validation.constraints.NotBlank;
 
 public record CommentReq(
-        @NotBlank Long postId, @NotBlank String content) {
+        @NotBlank String content) {
 }
