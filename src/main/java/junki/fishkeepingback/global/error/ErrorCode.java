1c83fd65e3143667dd9aeb9c4a8f8cd6eecdb4a6
hong jun ki <ghdwnsrl1991@gmail.com>
2024-12-16 18:26:44 +0900

    feat. 에러 처리 기능 구현

---
+++ src/main/java/junki/fishkeepingback/global/error/ErrorCode.java
@@ -0,0 +1,9 @@
+package junki.fishkeepingback.global.error;
+
+import org.springframework.http.HttpStatus;
+
+public interface ErrorCode {
+    String name();
+    HttpStatus getHttpStatus();
+    String getMessage();
+}
