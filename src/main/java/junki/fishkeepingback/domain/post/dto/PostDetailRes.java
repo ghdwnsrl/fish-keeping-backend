7f47f55504910fda30a271b716d59a6621583674
hong jun ki <ghdwnsrl1991@gmail.com>
2025-02-04 12:17:54 +0900

    fix. 단일 게시글 조회 api 수정

--- src/main/java/junki/fishkeepingback/domain/post/dto/PostDetailRes.java
+++ src/main/java/junki/fishkeepingback/domain/post/dto/PostDetailRes.java
@@ -1,27 +1,23 @@
 package junki.fishkeepingback.domain.post.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import junki.fishkeepingback.domain.comment.dto.CommentRes;
 import junki.fishkeepingback.domain.post.Post;
-import lombok.ToString;
-import org.springframework.data.domain.Page;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 public record PostDetailRes(
         Long id,
         String title,
         String content,
         String username,
-        String thumbnailUrl,
+        String userProfileImageUrl,
         Integer views,
         Boolean liked,
         @JsonFormat(pattern = "yyyy-MM-dd") LocalDateTime createdAt
 ) {
     public PostDetailRes(Post post, Boolean liked) {
         this(post.getId(), post.getTitle(), post.getContent(),
-                post.getUser().getUsername(), post.getThumbnailUrl(), post.getViews(), liked,
+                post.getUser().getUsername(), post.getUser().getProfileImageUrl(), post.getViews(), liked,
                 post.getCreatedAt());
     }
 }
